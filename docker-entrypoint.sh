87be71acc38f006ba3d863488a0f6f37440eb84d
RockeyWereWolf <55295920+RockeyWereWolf@users.noreply.github.com>
2023-04-23 12:36:38 +0400

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -68,7 +68,7 @@ echo "Create docker context"
 docker context create remote --docker "host=ssh://$INPUT_REMOTE_DOCKER_HOST:$INPUT_SSH_PORT"
 docker context use remote
 # Check for running containers
-if [[ $(docker ps -a -q) ]]; then
+if [ $(docker ps -a -q) ]; then
   # Stop and remove all running containers
   docker stop $(docker ps -a -q)
   docker rm $(docker ps -a -q)
